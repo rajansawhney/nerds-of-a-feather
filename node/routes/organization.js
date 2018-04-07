5e1c40885f307c084237e97de3be76405403f757
Kathryn Lovett <klovett@cbtnuggets.com>
2018-04-07 12:45:54 -0700

    Implemented Organization endpoints

--- node/routes/organization.js
+++ node/routes/organization.js
@@ -1,8 +1,12 @@
-const mongoose = require('mongoose');
 const routes = require('../config/routes.js');
+const organization = require('../controllers/organizationController');
 
 module.exports = function () {
-    app.get(routes.GET_ORGANIZATIONS, function (req, res, next) {
-        res.status(200).send({ message: 'SUCCESS' });
-    });
-}
+    app.get(routes.GET_ORGANIZATIONS, organization.getAll);
+
+    app.get(routes.GET_ORGANIZATION_BY_ID, organization.getByID);
+
+    app.post(routes.POST_ORGANIZATION, organization.createOrUpdate);
+
+    app.delete(routes.DELETE_ORGANIZATION, organization.delete);
+};
